d7663c1eabb3562797e9065b2a99b69e3e854b54
stenlyadam <stenly.adam@unklab.ac.id>
2025-02-05 09:22:09 +0800

    routing in nodejs

--- index.js
+++ index.js
@@ -3,11 +3,36 @@ const {hello, greetings} = require('./helloWorld')
 const moment = require('moment')
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.write(hello)
-    res.write(greetings())
-    res.write(moment().format('MMMM Do YYYY, h:mm:ss a'))
+    
+    // res.write(hello)
+    // res.write(greetings())
+    const url = req.url;
+    if(url === '/users'){
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/json')
+        res.write(JSON.stringify({
+            status: 'success',
+            message: 'Users',
+            date: moment().format('MMMM Do YYYY, h:mm:ss a')
+        }))
+    }
+    else if(url === '/post'){
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/json')
+        res.write(JSON.stringify({
+            status: 'success',
+            message: 'Post',
+            date: moment().format('MMMM Do YYYY, h:mm:ss a')
+        }))
+    } else {
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'text/json')
+        res.write(JSON.stringify({
+            status: 'not found',
+            message: 'Route tidak ditemukan',
+            date: moment().format('MMMM Do YYYY, h:mm:ss a')
+        }))
+    }
     res.end()
 })
 
