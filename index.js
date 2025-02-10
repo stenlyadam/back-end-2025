b68664d529bca096183d0fa815d46b98dced5b3b
stenlyadam <stenly.adam@unklab.ac.id>
2025-02-10 09:18:23 +0800

    pertemuan7: express js

--- index.js
+++ index.js
@@ -1,41 +1,16 @@
 const http = require('http')
 const {hello, greetings} = require('./helloWorld')
 const moment = require('moment')
+const express = require('express')
+const app = express()
 
-const server = http.createServer((req, res) => {
-    
-    // res.write(hello)
-    // res.write(greetings())
-    const url = req.url;
-    if(url === '/users'){
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'text/json')
-        res.write(JSON.stringify({
-            status: 'success',
-            message: 'Users',
-            date: moment().format('MMMM Do YYYY, h:mm:ss a')
-        }))
-    }
-    else if(url === '/post'){
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'text/json')
-        res.write(JSON.stringify({
-            status: 'success',
-            message: 'Post',
-            date: moment().format('MMMM Do YYYY, h:mm:ss a')
-        }))
-    } else {
-        res.statusCode = 404
-        res.setHeader('Content-Type', 'text/json')
-        res.write(JSON.stringify({
-            status: 'not found',
-            message: 'Route tidak ditemukan',
-            date: moment().format('MMMM Do YYYY, h:mm:ss a')
-        }))
-    }
-    res.end()
-})
+app.get('/', (req, res) => res.send('Hello World'))
+app.get('/about', (req, res) => res.status(200).json({
+    status: 'success',
+    message: 'About page',
+    data: []
+}))
 
 const hostname = "127.0.0.1"
 const port = 3000
-server.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}`))
+app.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}`))
