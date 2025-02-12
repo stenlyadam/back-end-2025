10b0dd2f9a3d7ede6c5f32e1f4ec6f7cf4ee4c84
stenlyadam <stenly.adam@unklab.ac.id>
2025-02-12 09:20:29 +0800

    Query String

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ app.all('/universal', (req, res) => res.send(`Request method ${req.method}`))
 // Routing dinamis 
 // 1. Menggunakan params
 app.get('/post/:id', (req, res)=> res.send(`Artikel ke - ${req.params.id}`))
+// 2. Menggunakan Query String
+app.get('/post', (req,res) => {
+    const {page, sort} = req.query;
+    res.send(`Query string= page :${page}, sort : ${sort}`)
+})
 
 const hostname = "127.0.0.1"
 const port = 3000
